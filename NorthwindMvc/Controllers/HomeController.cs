83ab64e0f5ae5ba0028fe27b0cb4fd593dd7f7a3
Nurguyan <centauri368@gmail.com>
2021-08-15 12:09:19 +0300

    Added products with price more than given, category page and async requests

--- NorthwindMvc/Controllers/HomeController.cs
+++ NorthwindMvc/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace NorthwindMvc.Controllers
 {
@@ -21,26 +23,26 @@ namespace NorthwindMvc.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             var model = new HomeIndexViewModel
             {
                 VisitorCount = (new Random()).Next(1, 1001),
-                Categories = _context.Categories.ToList(),
-                Products = _context.Products.ToList()
+                Categories = await _context.Categories.ToListAsync(),
+                Products = await _context.Products.ToListAsync()
             };
             return View(model);
         }
 
-        public IActionResult ProductDetail(int? id)
+        public async Task<IActionResult> ProductDetail(int? id)
         {
             if (!id.HasValue)
             {
                 return NotFound("You must pass a product ID in the route, for example, Home/ProductDetail/21");
             }
 
-            var model = _context.Products
-                .SingleOrDefault(p => p.ProductID == id);
+            var model = await _context.Products
+                .SingleOrDefaultAsync(p => p.ProductID == id);
 
             if (model == null)
             {
@@ -80,6 +82,43 @@ namespace NorthwindMvc.Controllers
             return View(model);
         }
 
-        
+        public IActionResult ProductsThatCostMoreThan(decimal? price)
+        {
+            if (!price.HasValue)
+            {
+                return NotFound("You must pass a product price in the query string, for example, / Home / ProductsThatCostMoreThan ? price = 50");
+            }
+            IEnumerable<Product> model = _context.Products
+            .Include(p => p.Category)
+            .Include(p => p.Supplier)
+            .Where(p => p.UnitPrice > price);
+            if (model.Count() == 0)
+            {
+                return NotFound($"No products cost more than {price:C}.");
+            }
+            ViewData["MaxPrice"] = price.Value.ToString("C");
+            return View(model); // pass model to view
+        }
+
+        [Route("Category/{id?}")]
+        public async Task<IActionResult> Category(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return NotFound("You must pass a product ID in the route, for example, Home/ProductDetail/21");
+            }
+            var model = await _context.Categories.SingleOrDefaultAsync(c => c.CategoryID == id);
+
+            if (model == null)
+            {
+                return NotFound($"Category with ID of {id} not found");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+
     }
 }
