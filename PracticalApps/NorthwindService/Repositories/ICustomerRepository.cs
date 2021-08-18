9df8397e60cb48157676d013dc34735a2b54b419
Nurguyan <centauri368@gmail.com>
2021-08-18 21:50:23 +0300

    added Customers CRUD API

---
+++ PracticalApps/NorthwindService/Repositories/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using Packt.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindService.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer> CreateAsync(Customer c);
+        Task<IEnumerable<Customer>> RetrieveAllAsync();
+        Task<Customer> RetrieveAsync(string id);
+        Task<Customer> UpdateAsync(string id, Customer c);
+        Task<bool?> DeleteAsync(string id);
+    }
+}
